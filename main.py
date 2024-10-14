49ae45b84b6a091f93987e323266b244cd45aafd
fabianodebastiani <fabiano.debastiani@gmail.com>
2024-10-14 01:55:00 -0300

    Potenciacao no main
    Adicionada uma chamada da funcao potenciacao no arquivo main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import lib
+import lib, lib2
 import autenticacao as aut
 
 print()
@@ -12,6 +12,7 @@ print()
 print("***********************************")
 print("O resultado da soma é: ", lib.soma(a, b))
 print("O resultado da subtração é: ", lib.subtracao(a, b))
+print("O resultado da potenciacao é: ", lib2.potenciacao(a, b)) 
 
 
 
